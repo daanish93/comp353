7ca2bae8dc9dd0ac8b837f5136148a319cfca1b0
williamkingbede <williamkingbede@gmail.com>
2018-11-20 19:28:30 -0500

    Small fix

--- create_client.php
+++ create_client.php
@@ -32,7 +32,7 @@
       <div class="nav-wrapper">
         <a href="#" class="brand-logo">Bank of Canada</a>
         <ul id="nav-mobile" class="right hide-on-med-and-down">
-          <li><a href="create_client.php">Register</a></li>
+          <li><a href="index.php">Login</a></li>
           <li><a href="admin.php">Admin</a></li>
         </ul>
       </div>
